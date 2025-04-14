f4b211faf4b6dc6602b247bb1acaac3017ea2fb5
gonzalopelizzari <gpelizzari@mail.austral.edu.ar>
2025-04-14 17:51:55 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,25 +1,20 @@
+import math
 def line():
-    import math
-    def calcular_y(a,b,x):
-        return a*x+b
-    def calcular_distancia(x1,y1,x2,y2):
-        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-    a= float(input("Ingrese el coeficiente A: "))
-    b= float(input("Ingrese el coeficiente B: "))
-    x1= float(input("Ingrese el valor de X1: "))
-    x2= float(input("Ingrese el valor de X2: "))
-    print(f"El coeficiente A de su ecuación de la recta es: {a}")
-    print(f"El coeficiente B de su ecuación de la recta es: {b}")
-    print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
-    print(f"El coeficiente X2 de su ecuación de la recta es: {x2}")
-    y1= a * x1  +  b
-    y2= a * x2  +  b
-    print(f"\nPara la siguiente ecuación:\n\tY = {a}X + {b}")
-    print("\nDados los siguientes puntos:")
-    print(f"\tP1 ({x1}, {y1})")
-    print(f"\tP2 ({x2}, {y2})")
-    if(x2>x1):
-        distancia= math.sqrt(((x2-x1)**2)+((y2-y1)**2))
-    else:
-        distancia= math.sqrt(((x1-x2)**2)+((y1-y2)**2))
-    print(f"\nLa distancia entre ellos es: {distancia}")
+    A = float(input("Ingrese el coeficiente A: "))
+    B = float(input("Ingrese el coeficiente B: "))
+    X1 = float(input("Ingrese el coeficiente X1: "))
+    X2 = float(input("Ingrese el coeficiente X2: "))
+    print("El coeficiente A de su ecuación de la recta es:", A)
+    print("El coeficiente B de su ecuación de la recta es:", B)
+    print("El coeficiente X1 de su ecuación de la recta es:",X1)
+    print("El coeficiente X2 de su ecuación de la recta es:",X2)
+    print('')
+    print("Para la siguiente ecuación:")
+    print(f'\tY = {A}X + {float(B)}')
+    print('')
+    print("Dados los siguientes puntos:")
+    print(f'\tP1 ({float(X1)}, {float(X1*A+B)})')
+    print(f'\tP2 ({float(X2)}, {float(X2*A+B)})')
+    print('')
+    distancia = math.dist([X1, X1*A+B], [X2, X2*A+B])
+    print(f"La distancia entre ellos es: {distancia}")
